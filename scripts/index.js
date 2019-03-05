2d0f9c29a99c36c1acca86d3f16326df1d557d9e
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-03-05 11:19:04 -0500

    cleaned up

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
 console.log("Do you copy?");
 console.log("Roger");
 
-const leetDictionary = {
-    A:  "4",
-    E:  "3",
-    G:  "6",
-    I:  "1",
-    O:  "0",
-    S:  "5",
-    T:  "7",
-};
-
 function translate(originalString) {
+    const leetDictionary = {
+        A:  "4",
+        E:  "3",
+        G:  "6",
+        I:  "1",
+        O:  "0",
+        S:  "5",
+        T:  "7",
+    };
     let result = "";
     for (let i = 0; i < originalString.length; i++) {
         let theLetter = originalString[i].toUpperCase();
