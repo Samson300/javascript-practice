88c38e367000dc53ff28d80340f6438c4c0be0a2
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-03-05 11:14:14 -0500

    finishes leetspeak translator

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,28 @@
-console.log("Do you copy?")
-console.log("Roger")
+console.log("Do you copy?");
+console.log("Roger");
 
 const leetDictionary = {
-    A:  4,
-    E:  3,
-    G:  6,
-    I:  1,
-    O:  0,
-    S:  5,
-    T:  7,
+    A:  "4",
+    E:  "3",
+    G:  "6",
+    I:  "1",
+    O:  "0",
+    S:  "5",
+    T:  "7",
 };
+
+function translate(originalString) {
+    let result = "";
+    for (let i = 0; i < originalString.length; i++) {
+        let theLetter = originalString[i].toUpperCase();
+        // console.log(theLetter);
+        // console.log(leetDictionary[theLetter]);
+        if (leetDictionary[theLetter]) {
+            result += leetDictionary[theLetter];
+        } else {
+            result += theLetter;
+        }
+    }
+    return result;
+}
+console.log(translate("hello dog"));
